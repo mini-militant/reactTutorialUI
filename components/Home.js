e1b4ef8ddd973e82ba7d109818192b71c50f186e
mini-militant <shailesh.coolcalm007@gmail.com>
2019-08-29 23:37:28 +0530

    1 file modified

--- components/Home.js
+++ components/Home.js
@@ -23,7 +23,7 @@ const HomePage=(props)=>{
   return(
     <div>
       <h2 className={classes.root}>What is React?</h2>
-      <div className={classes.content}>ReactJS tutorial provides basic and advanced concepts of ReactJS. Currently, ReactJS is one of the most popular JavaScript front-end libraries which has a strong foundation and a large community.ReactJS is a declarative, efficient, and flexible JavaScript library for building reusable UI components.</div>
+      <div className={classes.content}><b>ReactJS </b>tutorial provides basic and advanced concepts of ReactJS. Currently, ReactJS is one of the most popular JavaScript front-end libraries which has a strong foundation and a large community.ReactJS is a declarative, efficient, and flexible JavaScript library for building reusable UI components.</div>
 
       <div className={classes.content}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
 
